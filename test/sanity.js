613bf42d36e05f9b89464b906523e32f5a6e61dc
Jeremiah A. Bohling <jbohling@sycamoreleaf.com>
2018-06-08 14:44:53 -0500

    Simplify default testing

---
+++ test/sanity.js
@@ -0,0 +1,25 @@
+require('chai').should();
+
+describe('Hero file', function() {
+    var move;
+
+    beforeEach(function() {
+        move = require('../hero.js');
+    });
+
+    it('exists and exports a move function', function() {
+        move.should.be.a('function');
+    });
+});
+
+describe('Helper file', function() {
+    var helpers;
+
+    beforeEach(function() {
+        helpers = require('../helpers.js');
+    });
+
+    it('exists and exports a helper object', function() {
+        helpers.should.be.a('object');
+    });
+});
